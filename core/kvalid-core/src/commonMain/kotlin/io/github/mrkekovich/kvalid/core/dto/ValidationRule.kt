4a57612677026b3e43d52efa1091f391a0a44f16
MrKekovich <mrkekovich.official@gmail.com>
2024-07-14 14:29:17 +0300

    docs(NamedValue, ValidationRule)

--- core/kvalid-core/src/commonMain/kotlin/io/github/mrkekovich/kvalid/core/dto/ValidationRule.kt
+++ core/kvalid-core/src/commonMain/kotlin/io/github/mrkekovich/kvalid/core/dto/ValidationRule.kt
@@ -1,9 +1,22 @@
 package io.github.mrkekovich.kvalid.core.dto
 
+/**
+ * Represents a validation rule for a value.
+ *
+ * @param T the type of the value
+ * @property value the value to be validated
+ * @property message the error message if the validation fails
+ * @property predicate the predicate to validate the value
+ */
 data class ValidationRule<T>(
     val value: T,
     val message: String,
     val predicate: (T) -> Boolean,
 ) {
+    /**
+     * Validates the value against the predicate.
+     *
+     * @return true if the value is valid, false otherwise
+     */
     fun validate(): Boolean = predicate(value)
 }
