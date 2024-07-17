6c72b80eaa76fc70cf2acf3eb35382f3303026b2
MrKekovich <mrkekovich.official@gmail.com>
2024-07-17 12:03:22 +0300

    refactor(KValidContext): Add "KValidDslMarker" annotation to the interface.
    docs(KValidContext): Docstrings for KValidContext.rule, KValidContext.violation.

--- core/kvalid-core/src/commonMain/kotlin/io/github/mrkekovich/kvalid/core/context/KValidContext.kt
+++ core/kvalid-core/src/commonMain/kotlin/io/github/mrkekovich/kvalid/core/context/KValidContext.kt
@@ -3,17 +3,26 @@ package io.github.mrkekovich.kvalid.core.context
 import io.github.mrkekovich.kvalid.core.annotation.KValidDslMarker
 import io.github.mrkekovich.kvalid.core.dto.ValidationRule
 
+@KValidDslMarker
 interface KValidContext :
     NamedCollectionValidationContext,
     NamedComparableValidationContext,
     NamedStringValidationContext,
     ValidationContext {
-
-    @KValidDslMarker
+    /**
+     * Add validation rule
+     *
+     * @param message the failure message
+     * @param predicate the validation predicate
+     * @return the validation rule with [Unit] as [ValidationRule.value]
+     */
     fun rule(
         message: String,
         predicate: () -> Boolean,
     ): ValidationRule<Unit>
 
+    /**
+     * @param message the failure message
+     */
     fun violation(message: String)
 }
