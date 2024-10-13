25746e959c0a51094c0b979814f4daf91eacb7ff
mrkekovich <mrkekovich.official@gmail.com>
2024-10-13 17:06:11 +0300

    test(ValidationContextTest): Fix test.

--- projects/core/kvalid-core/src/commonTest/kotlin/org/kvalid/core/context/ValidationContextTest.kt
+++ projects/core/kvalid-core/src/commonTest/kotlin/org/kvalid/core/context/ValidationContextTest.kt
@@ -4,19 +4,25 @@ import io.kotest.assertions.shouldFail
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 import org.kvalid.core.context.Contexts.successContext
+import org.kvalid.core.model.NamedValue
 import org.kvalid.dsl.model.withName
 
 internal class ValidationContextTest : FunSpec({
     test("validation with message as lambda") {
+        val fieldName = "test"
+        val namedValue = "".withName(fieldName)
+
+        val message: (NamedValue<String>) -> String = { "$it must not be blank" }
+
         val error = shouldFail {
             successContext.run {
-                "".withName("test").validate(
-                    message = { "${it.name} must not be blank" },
+                namedValue.validate(
+                    message = message,
                     predicate = { it.isNotBlank() }
                 )
             }
         }
 
-        error.message shouldBe "Rule failed: test must not be blank"
+        error.message shouldBe message(namedValue)
     }
 })
