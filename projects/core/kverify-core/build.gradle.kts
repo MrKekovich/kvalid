76064c1e01257ed198b4ec7f7f6310ecd10feee6
mrkekovich <mrkekovich.official@gmail.com>
2024-10-19 10:48:59 +0300

    chore: Setup temporary publishing.

--- projects/core/kverify-core/build.gradle.kts
+++ projects/core/kverify-core/build.gradle.kts
@@ -1,10 +1,18 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.kotest.multiplatform)
+    alias(libs.plugins.maven.publish)
+    id("maven-publish")
+    id("signing")
 }
 
 kotlin {
     jvm {
+        compilations.all {
+            kotlinOptions.jvmTarget = "1.8"
+        }
         withJava()
     }
 
@@ -63,10 +71,51 @@ tasks.named<Test>("jvmTest") {
     testLogging {
         showExceptions = true
         showStandardStreams = true
-        events = setOf(
-            org.gradle.api.tasks.testing.logging.TestLogEvent.FAILED,
-            org.gradle.api.tasks.testing.logging.TestLogEvent.PASSED
-        )
+        events =
+            setOf(
+                org.gradle.api.tasks.testing.logging.TestLogEvent.FAILED,
+                org.gradle.api.tasks.testing.logging.TestLogEvent.PASSED,
+            )
         exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
     }
+}
+
+mavenPublishing {
+    val groupId: String by project
+    val kverifyVersion: String by project
+
+    coordinates(
+        groupId = "io.github.kverify",
+        artifactId = "kverify-core",
+        version = kverifyVersion,
+    )
+
+    pom {
+        name.set("KVerify")
+        description.set("KVerify - Kotlin Validation Library")
+        inceptionYear.set("2024")
+        url.set("https://github.com/kverify/kverify")
+
+        licenses {
+            license {
+                name.set("The Apache Software License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+
+        developers {
+            developer {
+                id.set("MrKekovich")
+                name.set("MrKekovich")
+                email.set("mrkekovich.official@gmail.com")
+            }
+        }
+
+        scm {
+            url.set("https://github.com/kverify/kverify")
+        }
+    }
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+
+    signAllPublications()
 }
