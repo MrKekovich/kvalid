32ea310bd5489f894d63272c40e5d12416458286
mrkekovich <mrkekovich.official@gmail.com>
2024-10-20 14:17:14 +0300

    chore: Setup build script.

--- projects/build.gradle.kts
+++ projects/build.gradle.kts
@@ -1,5 +1,24 @@
 plugins {
     alias(libs.plugins.kotlin.multiplatform).apply(false)
+    alias(libs.plugins.nexusPublish)
+}
+
+fun getProperty(variableName: String): String =
+    findProperty(variableName)?.toString()
+        ?: System.getenv(variableName)
+        ?: run {
+            println("!!! WARNING: Missing $variableName. Using default value.")
+            ""
+        }
+
+nexusPublishing {
+    repositories {
+        sonatype {
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+            username.set(getProperty("OSSRH_USERNAME"))
+            password.set(getProperty("OSSRH_PASSWORD"))
+        }
+    }
 }
 
 allprojects {
